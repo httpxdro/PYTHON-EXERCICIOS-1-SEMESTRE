03004508a6747297aeff28c74da43dcbd6d59c95
Pedro <91342788+httpxdro@users.noreply.github.com>
2022-05-03 08:56:50 -0300

    Add files via upload

---
+++ AULA 7/func1.py
@@ -0,0 +1,6 @@
+def saudacao (saudacao, nome):
+    print(f'{saudacao} {nome}')
+
+saudacao('Olá', 'João')
+saudacao('Olá', 'Maria')
+saudacao('Olá', 'Pedro')
---
+++ AULA 7/func2.py
@@ -0,0 +1,9 @@
+def soma (n1, n2, n3):
+    print(n1 + n2 + n3)
+
+
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o primeiro numero: '))
+n3 = int(input('Digite o primeiro numero: '))
+
+soma (n1, n2, n3)
